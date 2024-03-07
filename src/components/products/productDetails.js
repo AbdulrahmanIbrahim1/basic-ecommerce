6dc1a4e1778974edb8b7827157db771bd39891db
Abdulrahman Ibrahim <body01091532721@gmail.com>
2024-03-08 00:02:44 +0200

    ...

--- src/components/products/productDetails.js
+++ src/components/products/productDetails.js
@@ -8,7 +8,7 @@ function ProductDetails() {
   const [product, setproduct] = useState([])
   useEffect(() => {
     fetch(`${api_url}/${params.productId}`).then((res) => res.json()).then((product) => setproduct(product))
-  }, [])
+  }, [params.productId])
   return (<>
     <Product product={product} viewButton={false} />
   </>);
